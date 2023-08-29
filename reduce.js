390eb62b49b74812d3b060e34c30fe33d8a5678d
Daniel Morriss <danielmorriss1@gmail.com>
2023-08-29 12:39:20 +0100

    Add reduce.js

---
+++ reduce.js
@@ -0,0 +1,39 @@
+  
+/**
+ * To run this file in Gitpod, use the 
+ * command node reduce.js in the terminal
+ */
+
+
+// Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0); // ,0 is the initial item to start with, it's optional and if left out the fist item in the array will be used. Even though it's the same as the first value, it's good practice to include it.
+console.log(sum);
+
+const teamMembers = [
+    {
+      name: 'Andrew',
+      profession: 'Developer',
+      yrsExperience: 5
+    },
+    {
+      name: 'Ariel',
+      profession: 'Developer',
+      yrsExperience: 7
+    },
+    {
+      name: 'Michael',
+      profession: 'Designer',
+      yrsExperience: 1
+    },
+    {
+      name: 'Kelly',
+      profession: 'Designer',
+      yrsExperience: 3
+    }
+  ];
+  
+  // Totaling a specific object property
+  
+  
+  // Grouping by a property, and totaling it too
